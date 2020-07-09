f021546d3b07999b6217272efe39dd39914820e2
dpadron08 <padroncastillo@gmail.com>
2020-07-09 16:21:28 -0500

    Add adapter for displaying new layout in profile fragment

---
+++ app/src/main/java/com/example/parstagram/PostPreviewAdapter.java
@@ -0,0 +1,99 @@
+package com.example.parstagram;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class PostPreviewAdapter extends RecyclerView.Adapter<PostPreviewAdapter.ViewHolder> {
+    Context context;
+    private List<Post> posts;
+
+    public PostPreviewAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public PostPreviewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new PostPreviewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostPreviewAdapter.ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> list) {
+        posts.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivImage;
+
+        LinearLayout container;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(final Post post) {
+            // Bind the post data to the view elements
+            //tvDescription.setText(post.getDescription());
+            Log.i("ABC", "URL: " + post.getImage().getUrl());
+            //tvUsername.setText(post.getUser().getUsername());
+            ParseFile image = post.getImage();
+            if (image != null) {
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("AAA", "Clicked");
+                    // make sure the position is valid, i.e. actually exists in the view
+                    // create intent for the new activity
+                    Intent intent = new Intent(context, PostDetailsActivity.class);
+                    intent.putExtra("post", Parcels.wrap(post));
+                    context.startActivity(intent);
+
+                }
+            });
+        }
+
+    }
+}
